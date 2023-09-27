dbea34b90b48e6c94acb9666d88bc9136ac2ca2b
Berk Alp Direm <berkalpdirem@hotmail.com>
2023-09-27 11:59:16 +0300

    Switched to single form, multi panel logic

--- DietApp.PL/MainPage.Designer.cs
+++ DietApp.PL/MainPage.Designer.cs
@@ -28,164 +28,199 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
-            mp_btn_ListMeals = new Button();
-            mp_btn_DetailReports = new Button();
-            dataGridView = new DataGridView();
-            comboBox2 = new ComboBox();
-            comboBox3 = new ComboBox();
-            comboBox4 = new ComboBox();
-            mp_rb_Add = new RadioButton();
-            mp_rb_Update = new RadioButton();
-            mp_rb_Delete = new RadioButton();
-            mp_nup_Portion = new NumericUpDown();
-            ((System.ComponentModel.ISupportInitialize)dataGridView).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)mp_nup_Portion).BeginInit();
+            pnl_FlowPanel = new Panel();
+            mp_btn_Exit = new Button();
+            pnl_LoginPage = new Panel();
+            lg_btn_Register = new Button();
+            lp_btn_Entry = new Button();
+            lg_tb_Password = new TextBox();
+            lg_tb_Email = new TextBox();
+            pnl_RegisterPage = new Panel();
+            rp_btn_Back = new Button();
+            rp_btn_Register = new Button();
+            rp_tb_Password2 = new TextBox();
+            rp_tb_Password = new TextBox();
+            rp_tb_Email = new TextBox();
+            pnl_MainPage = new Panel();
+            pnl_FlowPanel.SuspendLayout();
+            pnl_LoginPage.SuspendLayout();
+            pnl_RegisterPage.SuspendLayout();
+            pnl_MainPage.SuspendLayout();
             SuspendLayout();
             // 
-            // button1
+            // pnl_FlowPanel
             // 
-            button1.Location = new Point(357, 35);
-            button1.Name = "button1";
-            button1.Size = new Size(161, 23);
-            button1.TabIndex = 0;
-            button1.Text = "Listele";
-            button1.UseVisualStyleBackColor = true;
+            pnl_FlowPanel.BackColor = Color.FromArgb(35, 40, 45);
+            pnl_FlowPanel.Controls.Add(mp_btn_Exit);
+            pnl_FlowPanel.Dock = DockStyle.Left;
+            pnl_FlowPanel.Location = new Point(0, 0);
+            pnl_FlowPanel.Name = "pnl_FlowPanel";
+            pnl_FlowPanel.Size = new Size(210, 450);
+            pnl_FlowPanel.TabIndex = 0;
             // 
-            // mp_btn_ListMeals
+            // mp_btn_Exit
             // 
-            mp_btn_ListMeals.Location = new Point(12, 280);
-            mp_btn_ListMeals.Name = "mp_btn_ListMeals";
-            mp_btn_ListMeals.Size = new Size(120, 40);
-            mp_btn_ListMeals.TabIndex = 0;
-            mp_btn_ListMeals.Text = "Öğünlerimi Listele";
-            mp_btn_ListMeals.UseVisualStyleBackColor = true;
+            mp_btn_Exit.Location = new Point(28, 413);
+            mp_btn_Exit.Name = "mp_btn_Exit";
+            mp_btn_Exit.Size = new Size(137, 23);
+            mp_btn_Exit.TabIndex = 0;
+            mp_btn_Exit.Text = "Çıkış";
+            mp_btn_Exit.UseVisualStyleBackColor = true;
+            mp_btn_Exit.Click += mp_btn_Exit_Click;
             // 
-            // mp_btn_DetailReports
+            // pnl_LoginPage
             // 
-            mp_btn_DetailReports.Location = new Point(12, 335);
-            mp_btn_DetailReports.Name = "mp_btn_DetailReports";
-            mp_btn_DetailReports.Size = new Size(120, 40);
-            mp_btn_DetailReports.TabIndex = 0;
-            mp_btn_DetailReports.Text = "Detaylı Raporlar";
-            mp_btn_DetailReports.UseVisualStyleBackColor = true;
+            pnl_LoginPage.BackColor = SystemColors.ButtonShadow;
+            pnl_LoginPage.Controls.Add(lg_btn_Register);
+            pnl_LoginPage.Controls.Add(lp_btn_Entry);
+            pnl_LoginPage.Controls.Add(lg_tb_Password);
+            pnl_LoginPage.Controls.Add(lg_tb_Email);
+            pnl_LoginPage.Dock = DockStyle.Fill;
+            pnl_LoginPage.Location = new Point(0, 0);
+            pnl_LoginPage.Name = "pnl_LoginPage";
+            pnl_LoginPage.Size = new Size(800, 450);
+            pnl_LoginPage.TabIndex = 1;
             // 
-            // dataGridView
+            // lg_btn_Register
             // 
-            dataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView.Location = new Point(145, 89);
-            dataGridView.Name = "dataGridView";
-            dataGridView.RowTemplate.Height = 25;
-            dataGridView.Size = new Size(377, 282);
-            dataGridView.TabIndex = 1;
+            lg_btn_Register.Location = new Point(383, 218);
+            lg_btn_Register.Name = "lg_btn_Register";
+            lg_btn_Register.Size = new Size(100, 23);
+            lg_btn_Register.TabIndex = 4;
+            lg_btn_Register.Text = "Kayıt Ol";
+            lg_btn_Register.UseVisualStyleBackColor = true;
+            lg_btn_Register.Click += lg_btn_Register_Click;
             // 
-            // comboBox2
+            // lp_btn_Entry
             // 
-            comboBox2.FormattingEnabled = true;
-            comboBox2.Location = new Point(357, 59);
-            comboBox2.Name = "comboBox2";
-            comboBox2.Size = new Size(100, 23);
-            comboBox2.TabIndex = 2;
+            lp_btn_Entry.Location = new Point(277, 218);
+            lp_btn_Entry.Name = "lp_btn_Entry";
+            lp_btn_Entry.Size = new Size(100, 23);
+            lp_btn_Entry.TabIndex = 3;
+            lp_btn_Entry.Text = "Giriş";
+            lp_btn_Entry.UseVisualStyleBackColor = true;
+            lp_btn_Entry.Click += lp_btn_Entry_Click;
             // 
-            // comboBox3
+            // lg_tb_Password
             // 
-            comboBox3.FormattingEnabled = true;
-            comboBox3.Location = new Point(251, 59);
-            comboBox3.Name = "comboBox3";
-            comboBox3.Size = new Size(100, 23);
-            comboBox3.TabIndex = 2;
+            lg_tb_Password.Location = new Point(277, 189);
+            lg_tb_Password.Name = "lg_tb_Password";
+            lg_tb_Password.PlaceholderText = "Şifre";
+            lg_tb_Password.Size = new Size(206, 23);
+            lg_tb_Password.TabIndex = 2;
             // 
-            // comboBox4
+            // lg_tb_Email
             // 
-            comboBox4.FormattingEnabled = true;
-            comboBox4.Location = new Point(145, 60);
-            comboBox4.Name = "comboBox4";
-            comboBox4.Size = new Size(100, 23);
-            comboBox4.TabIndex = 2;
-            comboBox4.Text = "Numara";
+            lg_tb_Email.Location = new Point(277, 160);
+            lg_tb_Email.Name = "lg_tb_Email";
+            lg_tb_Email.PlaceholderText = "E-Mail";
+            lg_tb_Email.Size = new Size(206, 23);
+            lg_tb_Email.TabIndex = 1;
             // 
-            // mp_rb_Add
+            // pnl_RegisterPage
             // 
-            mp_rb_Add.AutoSize = true;
-            mp_rb_Add.Location = new Point(145, 35);
-            mp_rb_Add.Name = "mp_rb_Add";
-            mp_rb_Add.Size = new Size(46, 19);
-            mp_rb_Add.TabIndex = 3;
-            mp_rb_Add.TabStop = true;
-            mp_rb_Add.Text = "Ekle";
-            mp_rb_Add.UseVisualStyleBackColor = true;
+            pnl_RegisterPage.BackColor = SystemColors.ButtonShadow;
+            pnl_RegisterPage.Controls.Add(rp_btn_Back);
+            pnl_RegisterPage.Controls.Add(rp_btn_Register);
+            pnl_RegisterPage.Controls.Add(rp_tb_Password2);
+            pnl_RegisterPage.Controls.Add(rp_tb_Password);
+            pnl_RegisterPage.Controls.Add(rp_tb_Email);
+            pnl_RegisterPage.Dock = DockStyle.Fill;
+            pnl_RegisterPage.Enabled = false;
+            pnl_RegisterPage.Location = new Point(0, 0);
+            pnl_RegisterPage.Name = "pnl_RegisterPage";
+            pnl_RegisterPage.Size = new Size(800, 450);
+            pnl_RegisterPage.TabIndex = 2;
             // 
-            // mp_rb_Update
+            // rp_btn_Back
             // 
-            mp_rb_Update.AutoSize = true;
-            mp_rb_Update.Location = new Point(197, 35);
-            mp_rb_Update.Name = "mp_rb_Update";
-            mp_rb_Update.Size = new Size(71, 19);
-            mp_rb_Update.TabIndex = 3;
-            mp_rb_Update.TabStop = true;
-            mp_rb_Update.Text = "Güncelle";
-            mp_rb_Update.UseVisualStyleBackColor = true;
+            rp_btn_Back.Location = new Point(391, 229);
+            rp_btn_Back.Name = "rp_btn_Back";
+            rp_btn_Back.Size = new Size(70, 23);
+            rp_btn_Back.TabIndex = 5;
+            rp_btn_Back.Text = " Geri";
+            rp_btn_Back.UseVisualStyleBackColor = true;
+            rp_btn_Back.Click += rg_btn_Back_Click;
             // 
-            // mp_rb_Delete
+            // rp_btn_Register
             // 
-            mp_rb_Delete.AutoSize = true;
-            mp_rb_Delete.Location = new Point(274, 35);
-            mp_rb_Delete.Name = "mp_rb_Delete";
-            mp_rb_Delete.Size = new Size(37, 19);
-            mp_rb_Delete.TabIndex = 3;
-            mp_rb_Delete.TabStop = true;
-            mp_rb_Delete.Text = "Sil";
-            mp_rb_Delete.UseVisualStyleBackColor = true;
+            rp_btn_Register.Location = new Point(315, 229);
+            rp_btn_Register.Name = "rp_btn_Register";
+            rp_btn_Register.Size = new Size(70, 23);
+            rp_btn_Register.TabIndex = 4;
+            rp_btn_Register.Text = "Kayıt Ol";
+            rp_btn_Register.UseVisualStyleBackColor = true;
+            rp_btn_Register.Click += rg_btn_Register_Click;
             // 
-            // mp_nup_Portion
+            // rp_tb_Password2
             // 
-            mp_nup_Portion.DecimalPlaces = 1;
-            mp_nup_Portion.Increment = new decimal(new int[] { 5, 0, 0, 65536 });
-            mp_nup_Portion.Location = new Point(463, 60);
-            mp_nup_Portion.Maximum = new decimal(new int[] { 10, 0, 0, 0 });
-            mp_nup_Portion.Name = "mp_nup_Portion";
-            mp_nup_Portion.Size = new Size(59, 23);
-            mp_nup_Portion.TabIndex = 4;
+            rp_tb_Password2.Location = new Point(315, 200);
+            rp_tb_Password2.Name = "rp_tb_Password2";
+            rp_tb_Password2.PlaceholderText = "Şifrenizi Tekrardan Giriniz";
+            rp_tb_Password2.Size = new Size(146, 23);
+            rp_tb_Password2.TabIndex = 3;
+            // 
+            // rp_tb_Password
+            // 
+            rp_tb_Password.Location = new Point(315, 171);
+            rp_tb_Password.Name = "rp_tb_Password";
+            rp_tb_Password.PlaceholderText = "Şifrenizi Giriniz";
+            rp_tb_Password.Size = new Size(146, 23);
+            rp_tb_Password.TabIndex = 2;
+            // 
+            // rp_tb_Email
+            // 
+            rp_tb_Email.Location = new Point(315, 142);
+            rp_tb_Email.Name = "rp_tb_Email";
+            rp_tb_Email.PlaceholderText = "E-Mailinizi Giriniz";
+            rp_tb_Email.Size = new Size(146, 23);
+            rp_tb_Email.TabIndex = 1;
+            // 
+            // pnl_MainPage
+            // 
+            pnl_MainPage.BackColor = SystemColors.ButtonShadow;
+            pnl_MainPage.Controls.Add(pnl_FlowPanel);
+            pnl_MainPage.Dock = DockStyle.Fill;
+            pnl_MainPage.Location = new Point(0, 0);
+            pnl_MainPage.Name = "pnl_MainPage";
+            pnl_MainPage.Size = new Size(800, 450);
+            pnl_MainPage.TabIndex = 2;
             // 
             // MainPage
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(530, 379);
-            Controls.Add(mp_nup_Portion);
-            Controls.Add(mp_rb_Delete);
-            Controls.Add(mp_rb_Update);
-            Controls.Add(mp_rb_Add);
-            Controls.Add(comboBox4);
-            Controls.Add(comboBox3);
-            Controls.Add(comboBox2);
-            Controls.Add(dataGridView);
-            Controls.Add(mp_btn_DetailReports);
-            Controls.Add(mp_btn_ListMeals);
-            Controls.Add(button1);
+            ClientSize = new Size(800, 450);
+            Controls.Add(pnl_MainPage);
+            Controls.Add(pnl_LoginPage);
+            Controls.Add(pnl_RegisterPage);
             Name = "MainPage";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "MainPage";
-            FormClosed += MainPage_FormClosed;
-            Load += MainPage_Load;
-            ((System.ComponentModel.ISupportInitialize)dataGridView).EndInit();
-            ((System.ComponentModel.ISupportInitialize)mp_nup_Portion).EndInit();
+            pnl_FlowPanel.ResumeLayout(false);
+            pnl_LoginPage.ResumeLayout(false);
+            pnl_LoginPage.PerformLayout();
+            pnl_RegisterPage.ResumeLayout(false);
+            pnl_RegisterPage.PerformLayout();
+            pnl_MainPage.ResumeLayout(false);
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
-        private Button mp_btn_UserProfile;
-        private Button button1;
-        private Button mp_btn_ListMeals;
-        private Button mp_btn_DetailReports;
-        private DataGridView dataGridView;
-        private ComboBox comboBox2;
-        private ComboBox comboBox3;
-        private ComboBox comboBox4;
-        private RadioButton mp_rb_Add;
-        private RadioButton mp_rb_Update;
-        private RadioButton mp_rb_Delete;
-        private NumericUpDown mp_nup_Portion;
+
+        private Panel pnl_FlowPanel;
+        private Panel pnl_LoginPage;
+        private Button lg_btn_Register;
+        private Button lp_btn_Entry;
+        private TextBox lg_tb_Password;
+        private TextBox lg_tb_Email;
+        private Panel pnl_RegisterPage;
+        private Button rp_btn_Back;
+        private Button rp_btn_Register;
+        private TextBox rp_tb_Password2;
+        private TextBox rp_tb_Password;
+        private TextBox rp_tb_Email;
+        private Panel pnl_MainPage;
+        private Button mp_btn_Exit;
     }
 }
--- DietApp.PL/MainPage.cs
+++ DietApp.PL/MainPage.cs
@@ -15,19 +15,60 @@ namespace DietApp.PL
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        //Login Page Actions
+
+        private void lp_btn_Entry_Click(object sender, EventArgs e)
+        {
+            if (lg_tb_Email.Text == "alp" && lg_tb_Password.Text == "alp123")
+            {
+                lg_tb_Email.Clear();
+                lg_tb_Password.Clear();
+                pnl_LoginPage.Enabled = false;
+                pnl_MainPage.Enabled = true;
+                pnl_MainPage.BringToFront();
+            }
+            else
+            {
+                MessageBox.Show("Giriş Bilgileriniz Hatalı Lütfen Tekrardan Deneyiniz!!!");
+            }
+        }
+        private void lg_btn_Register_Click(object sender, EventArgs e)
+        {
+            lg_tb_Email.Clear();
+            lg_tb_Password.Clear();
+            pnl_RegisterPage.BringToFront();
+            pnl_LoginPage.Enabled = false;
+            pnl_RegisterPage.Enabled = true;
+        }
+
+        //Register Page Actions
+        private void rg_btn_Register_Click(object sender, EventArgs e)
+        {
+            rp_tb_Email.Clear();
+            rp_tb_Password.Clear();
+            rp_tb_Password2.Clear();
+            pnl_RegisterPage.Enabled = false;
+            pnl_LoginPage.Enabled = true;
+            pnl_LoginPage.BringToFront();
+        }
+        private void rg_btn_Back_Click(object sender, EventArgs e)
+        {
+            rp_tb_Email.Clear();
+            rp_tb_Password.Clear();
+            rp_tb_Password2.Clear();
+            pnl_RegisterPage.Enabled = false;
+            pnl_LoginPage.Enabled = true;
+            pnl_LoginPage.BringToFront();
 
         }
 
-
-
-        private void MainPage_FormClosed(object sender, FormClosedEventArgs e)
+        //Flow Panel Actions
+        private void mp_btn_Exit_Click(object sender, EventArgs e)
         {
-            Application.Exit();
-        }
-
-        private void MainPage_Load(object sender, EventArgs e)
-        {
-
+            pnl_LoginPage.Enabled = true;
+            pnl_LoginPage.BringToFront();
         }
     }
 }
